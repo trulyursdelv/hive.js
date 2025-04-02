bed57748074643e87626bd2672e9b4bd3894af16
delv 𖹭 <151720755+trulyursdelv@users.noreply.github.com>
2025-04-02 11:48:25 +0800

    Add files via upload

--- dist/hive.js
+++ dist/hive.js
@@ -3,14 +3,13 @@
 
 (() => {
 
-// var: y
-const hive_cache = {
-  _loaded_cache: [],
-  _is_cache_loaded: false,
-  _init_cache() {
-    if(hive._is_cache_loaded) return;
-    hive._is_cache_loaded = true;
-    const list = localStorage.getItem("hive-cache:all") || "[]";
+const hive_state = {
+  _loaded_state: [],
+  _is_state_loaded: false,
+  _init_states() {
+    if(hive._is_state_loaded) return;
+    hive._is_state_loaded = true;
+    const list = localStorage.getItem("hive-state:all") || "[]";
     let destroy = [];
     JSON.parse(list).forEach((path, i) => {
       const content = JSON.parse(localStorage.getItem(path));
@@ -18,20 +17,20 @@ const hive_cache = {
         destroy.push(path);
         localStorage.removeItem(path);
       } else {
-        hive._loaded_cache.push(content);
+        hive._loaded_state.push(content);
       }
     });
-    localStorage.setItem("hive-cache:all", JSON.stringify(list.filter(path => !destroy.includes(path))));
+    localStorage.setItem("hive-state:all", JSON.stringify(list.filter(path => !destroy.includes(path))));
   },
-  _update_cache() {
-    localStorage.setItem("hive-cache:all", JSON.stringify(hive._loaded_cache.map(y => y.path)));
+  _update_state() {
+    localStorage.setItem("hive-state:all", JSON.stringify(hive._loaded_state.map(y => y.path)));
   },
-  cache(key, placeholder) {
+  state(key, placeholder) {
     let is_loaded = false;
     let value = placeholder;
-    let path = `hive-cache:${crypto.randomUUID()}`;
-    hive._init_cache();
-    hive._loaded_cache.forEach(y => {
+    let path = `hive-state:${crypto.randomUUID()}`;
+    hive._init_states();
+    hive._loaded_state.forEach(y => {
       if(y.key != key) return;
       value = y.value;
       path = y.path;
@@ -43,39 +42,158 @@ const hive_cache = {
         // 5 hours
         expire: Date.now() + 18e6
       }
-      hive._loaded_cache.push(content);
+      hive._loaded_state.push(content);
       localStorage.setItem(path, JSON.stringify(content));
     }
     const read = (placeholder) => {
-      return hive.read_cache(key, placeholder);
+      const state = hive._loaded_state.find(y => y.key == key);
+      return state ? state.value : placeholder;
     }
     const write = (value) => {
-      hive.write_cache(key, value);
+      hive._loaded_state.forEach((y, i) => {
+        if(y.key != key) return;
+        const state = hive._loaded_state[i];
+        hive._loaded_state[i].value = value;
+        localStorage.setItem(state.path, JSON.stringify({
+          key: state.key,
+          value, path: state.path
+        }));
+        hive._update_state();
+      });
     }
     return [read, write];
+  }
+}
+
+const hive_database = {
+  _database: null,
+  async connect(name, version=1) {
+    return new Promise((resolve, reject) => {
+      const request = window.indexedDB.open(name, version);
+      request.onupgradeneeded = evt => {
+        const db = evt.target.result;
+        const main = db.createObjectStore("main", { keyPath: "key" });
+        main.createIndex("value", "value", { unique: false });
+      }
+      request.onsuccess = evt => {
+        resolve(() => {
+          hive._database = evt.target.result
+        });
+      }
+      request.onerror = evt => {
+        reject(evt.target.error);
+      }
+    });
   },
-  read_cache(key, placeholder) {
-    const cache = hive._loaded_cache.find(y => y.key == key);
-    return cache ? cache.value : placeholder;
+  async store(key, value, define) {
+    const db = hive._database;
+    return new Promise((resolve, reject) => {
+      const trans = db.transaction(["main"], "readwrite");
+      const store = trans.objectStore("main");
+      const request = store.get(key);
+      request.onsuccess = () => {
+        const result = request.result;
+        if(result != null) {
+          result.value = value;
+          if(define) window[define] = value;
+          store.put(result);
+        } else {
+          store.put({ key, value });
+        }
+        resolve();
+      }
+      request.onerror = () => {
+        reject(request.error);
+      }
+    });
   },
-  write_cache(key, value) {
-    hive._loaded_cache.forEach((y, i) => {
-      if(y.key != key) return;
-      const cache = hive._loaded_cache[i];
-      hive._loaded_cache[i].value = value;
-      localStorage.setItem(cache.path, JSON.stringify({
-        key: cache.key,
-        value, path: cache.path
-      }));
-      hive._update_cache();
+  retrieve(key, placeholder, define) {
+    const db = hive._database;
+    return new Promise((resolve, reject) => {
+      const trans = db.transaction(["main"], "readonly");
+      const store = trans.objectStore("main");
+      const request = store.get(key);
+      request.onsuccess = () => {
+        const result = request.result;
+        const value = result?.value || placeholder;
+        if(define) window[define] = value;
+        resolve(value);
+      }
+      request.onerror = () => {
+        reject(request.error);
+      }
     });
   }
 }
 
-// var: none
+const hive_fields = {
+  fields: {},
+  _fields_page: {},
+  _init_fields() {
+    const fields = [];
+    document.querySelectorAll("[data-page]").forEach(page => {
+      page.querySelectorAll("[data-field]").forEach(field => {
+        const label = field.getAttribute("data-field");
+        field.addEventListener("change", (evt) => {
+          hive.fields[label] = evt.target.value;
+        });
+        hive.fields[label] = field.value;
+        fields.push({
+          target: field, label,
+          reset: field.value
+        });
+        hive._fields_page[page].push(label);
+      });
+    });
+  },
+  _update_fields(ignore) {
+    for(let page in hive._fields_page) {
+      if(page == ignore) continue;
+      hive._fields_page[page].forEach(f => {
+        f.target.value = f.reset;
+      });
+    }
+  }
+}
+
+const hive_pages = {
+  _pages: [],
+  _init_pages() {
+    document.querySelectorAll("[data-page]").forEach(f => {
+      const name = f.getAttribute("data-page");
+      hive._pages.push({
+        target: f, name,
+        is_active: f.hasAttribute("data-active"),
+        show(target) {
+          target.style.display = "block";
+        },
+        hide(target) {
+          target.style.display = "none";
+        }
+      });
+      hive.fields[name] = [];
+    });
+  },
+  configure_page(name, { show, hide }) {
+    hive._pages.forEach((f, i) => {
+      if(f.name != name) return;
+      if(show) hive._pages[i].show = show;
+      if(hide) hive._pages[i].hide = hide;
+    });
+  },
+  navigate(name) {
+    hive._pages.forEach(f => {
+      if(f.name == name) {
+        hive._update_fields(name);
+        f.show();
+      } else f.hide();
+    })
+  }
+}
+
 const hive_models = {
   _models: [],
-  createModel(name, handler, fallback) {
+  create_model(name, handler, fallback) {
     hive._models.push({
       name, handler, fallback
     });
@@ -84,7 +202,7 @@ const hive_models = {
     const model = hive._models.find(y => y.name == name);
     return model ? model.handler(payload) : null;
   },
-  mapModel(name, payloads) {
+  mount(name, payloads) {
     const model = hive._models.find(y => y.name == name);
     if(payloads.length == 0) {
       return model.fallback();
@@ -94,134 +212,19 @@ const hive_models = {
   }
 }
 
-// var: f
-const hive_pages = {
-  _pages: [],
-  _init_pages() {
-    hive._pages = [...document.querySelectorAll("[data-page]")].map(f => {
-      return {
-        target: f,
-        name: f.getAttribute("data-page"),
-        is_active: f.hasAttribute("data-active"),
-        show(target) {
-          target.style.display = "block";
-        },
-        hide(target) {
-          target.style.display = "none";
-        },
-        on() {}
-      }
-    });
-  },
-  configure_page(name, { show, hide, on }) {
-    hive._pages.forEach((f, i) => {
-      if(f.name != name) return;
-      if(show) hive._pages[i].show = show;
-      if(hide) hive._pages[i].hide = hide;
-      if(on) hive._pages[i].on = on;
-    });
-  },
-  navigate(name) {
-    hive._pages.forEach(f => {
-      if(f.name == name) {
-        f.show();
-        f.on();
-      } else {
-        f.hide();
-      }
-    })
-  }
-}
-
-// var: k
-const hive_fields = {
-  _fields: [],
-  _init_fields() {
-    hive._fields = [...document.querySelectorAll("[data-field]")].map(k => {
-      return {
-        target: k,
-        label: k.getAttribute("data-field"),
-        change() {},
-        reset() {
-          return k.value
-        }
-      }
-    });
-    hive._update_fields();
-  },
-  _update_fields() {
-    hive._fields.forEach(k => {
-      k.target.addEventListener("change", (evt) => {
-        k.change(evt.target.value);
-      });
-    });
-  },
-  reset_fields(labels) {
-    hive._fields.forEach((k, i) => {
-      if(!labels.includes(k.label)) return;
-      k.target.value = k.reset();
-    });
-  },
-  configure_field(label, { change, reset }) {
-    hive._fields.forEach((k, i) => {
-      if(k.label != label) return;
-      if(change) hive._fields[i].change = show;
-      if(reset) hive._pages[i].reset = reset;
-      hive._update_fields();
-    });
-  }
-}
-
-// var: v
-const hive_databases = {
-  _database: null,
-  use_database(v) {
-    hive._database = v;
-  },
-  async store(name, value) {
-    await hive._database.set(name, value);
-  },
-  async retrieve(name, placeholder) {
-    return await hive._database.get(name, placeholder);
-  },
-  async store_from(name, cache) {
-    await hive.store(name, hive.read_cache(cache));
-  },
-  async retrive_from(name, mapping) {
-    for(let db_key in mapping) {
-      let cache_key = mapping[db_key];
-      hive.write_cache(cache_key, await hive.retrieve(db_key));
-    }
-  }
-}
-
-// var: z
-const hive_modules = {
-  _exports: [],
-  export(name, builder) {
-    hive._exports.push({
-      name, builder
-    });
-  },
-  import(name, payload) {
-    const module = hive._exports.find(z => z.name == name);
-    if(module) module.builder(payload);
-  }
-}
-
 window.hive = {
-  version: 1,
+  version: 1.1,
   init() {
-    hive._init_cache();
+    hive._init_states();
     hive._init_pages();
     hive._init_fields();
+    hive._init_externals();
   },
-  ...hive_cache,
-  ...hive_models,
+  ...hive_state,
+  ...hive_database,
   ...hive_pages,
   ...hive_fields,
-  ...hive_databases,
-  ...hive_modules
+  ...hive_models
 }
 
 })();
